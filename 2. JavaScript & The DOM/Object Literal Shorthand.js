2939e9edee9f16b080d1b370d76aa11d688a5885
sangumee <super2451894@gmail.com>
2018-08-10 14:07:45 +0900

    Object Literal Shorthand
    Object Literal Shorthand

---
+++ 2. JavaScript & The DOM/Object Literal Shorthand.js
@@ -0,0 +1,12 @@
+let type = 'quartz';
+let color = 'rose';
+let carat = 21.29;
+
+const gemstone = {
+  type,
+  color,
+  carat,
+  calculateWorth() {
+    // will calculate worth of gemstone based on type, color, and carat
+  }
+};

19b0fc568868200af86165e062a763c2ae45613d
sangumee <super2451894@gmail.com>
2018-08-10 14:29:41 +0900

    Quiz - Writing a For...of Loop
    Quiz - Writing a For...of Loop

---
+++ 2. JavaScript & The DOM/Quiz - Writing a For...of Loop.js
@@ -0,0 +1,27 @@
+/*
+ * Programming Quiz: Writing a For...of Loop (1-4)
+ */
+
+// Directions:
+// Write a for...of loop that:
+
+// loops through each day in the days array
+// capitalizes the first letter of the day
+// and prints the day out to the console
+// Your code should log the following to the console:
+
+// Sunday
+// Monday
+// Tuesday
+// Wednesday
+// Thursday
+// Friday
+// Saturday
+
+const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+
+// your code goes here
+for (let day of days) {
+    day = day[0].toUpperCase() + day.slice(1);
+    console.log(day);
+}

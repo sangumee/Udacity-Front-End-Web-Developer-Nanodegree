c30af7e3778dc564180fb7f014c225270e79358c
sangumee <super2451894@gmail.com>
2018-08-10 14:58:39 +0900

    Spread Operator
    Spread Operator

---
+++ 2. JavaScript & The DOM/Spread... Operator.js
@@ -0,0 +1,12 @@
+const books = ["Don Quixote", "The Hobbit", "Alice in Wonderland", "Tale of Two Cities"];
+console.log(...books);
+
+const primes = new Set([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
+console.log(...primes);
+
+// Array’s concat() method.
+const fruits = ["apples", "bananas", "pears"];
+const vegetables = ["corn", "potatoes", "carrots"];
+const produce = fruits.concat(vegetables);
+console.log(produce);
+

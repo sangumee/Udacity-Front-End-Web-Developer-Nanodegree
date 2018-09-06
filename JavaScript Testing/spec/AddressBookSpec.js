5ffc38d8ae5d6c42a0d18686876b5d9617bbc36e
sangumee <super2451894@gmail.com>
2018-09-06 11:51:51 +0900

    beautify the code
    beautify the code

--- JavaScript Testing/spec/AddressBookSpec.js
+++ JavaScript Testing/spec/AddressBookSpec.js
@@ -1,32 +1,32 @@
 describe('Address Book', function () {
-        let addressBook,
+    let addressBook,
         thisContact;
 
-        beforeEach(function(){
-            addressBook=new AddressBook();
-            thisContact=new Contact();
-        });
+    beforeEach(function () {
+        addressBook = new AddressBook();
+        thisContact = new Contact();
+    });
 
-        it('should be able to add a contact', function(){
-            addressBook.addContact(thisContact);
-            expect(addressBook.getContact(0)).toBe(thisContact);
-        });
-        it('should be able to delete a contact', function(){
-            addressBook.addContact(thisContact);
-            addressBook.deleteContact(0);
-            expect(addressBook.getContact(0)).not.toBeDefined();
-        });
+    it('should be able to add a contact', function () {
+        addressBook.addContact(thisContact);
+        expect(addressBook.getContact(0)).toBe(thisContact);
+    });
+    it('should be able to delete a contact', function () {
+        addressBook.addContact(thisContact);
+        addressBook.deleteContact(0);
+        expect(addressBook.getContact(0)).not.toBeDefined();
+    });
 });
 
-describe('Async Address Book', function(){
-    let addressBook =new AddressBook();
+describe('Async Address Book', function () {
+    let addressBook = new AddressBook();
 
-    beforeEach(function(done){
-        addressBook.getInitialContacts(function(){
+    beforeEach(function (done) {
+        addressBook.getInitialContacts(function () {
             done();
         });
     });
-    it('should grab initial contacts', function(done){
+    it('should grab initial contacts', function (done) {
         expect(addressBook.initialComplete).toBe(true);
         done();
     });
